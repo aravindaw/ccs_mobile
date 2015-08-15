ca2c9f8e93517ffca038c7375480b0fcd23c1bd3
Aravinda <aravindaweerasekara@gmail.com>
2015-08-16 01:26:28 +0530

    adding loading spinner and smsc layout modifications.

--- app/src/main/java/com/hms/aravinda/smsc_customer_care_mobile/SmscMainFragment.java
+++ app/src/main/java/com/hms/aravinda/smsc_customer_care_mobile/SmscMainFragment.java
@@ -1,5 +1,7 @@
 package com.hms.aravinda.smsc_customer_care_mobile;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,6 +14,7 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,6 +23,11 @@ import java.util.List;
 public class SmscMainFragment extends Fragment {
 
     View rootview;
+    private View mContentView;
+    private View mLoadingView;
+    private int mShortAnimationDuration;
+
+    //smsc list
     public String smscList = "http://www.mocky.io/v2/55b9182bacd73ab008ee2358";
     //smsc1
     public String smscDetails = "http://www.mocky.io/v2/55b8d0dbacd73a9702ee2305";
@@ -27,6 +35,7 @@ public class SmscMainFragment extends Fragment {
     public String smscDetails2 = "http://www.mocky.io/v2/55b8d0ffacd73a9702ee2306";
     //smsc3
     public String smscDetails3 = "http://www.mocky.io/v2/55b8d120acd73a9c02ee2307";
+    //smsc session details
     public String smscSessionDetails = "http://www.mocky.io/v2/55b82101f8bc809a1101c4bd";
     public String executeURL;
 
@@ -36,6 +45,16 @@ public class SmscMainFragment extends Fragment {
 
         rootview = inflater.inflate(R.layout.smsc_layout, container, false);
 
+        mContentView = rootview.findViewById(R.id.smsc_linearLayout);
+        mLoadingView = rootview.findViewById(R.id.loading_spinner);
+
+        // Initially hide the content view.
+        mContentView.setVisibility(View.GONE);
+
+        // Retrieve and cache the system's default "short" animation time.
+        mShortAnimationDuration = getResources().getInteger(
+                android.R.integer.config_shortAnimTime);
+
         new AsyncTasksManager(new OnTaskCompleted() {
             @Override
             public void onTaskCompleted(List s) {
@@ -43,12 +62,14 @@ public class SmscMainFragment extends Fragment {
                 ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, s);
                 dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 smscList.setAdapter(dataAdapter);
+                showContentOrLoadingIndicator(true);
             }
         }).execute(smscList);
         View btnSmscDetails = rootview.findViewById(R.id.getSmscs);
         btnSmscDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                showContentOrLoadingIndicator(false);
                 new AsyncTasksManager(new OnTaskCompleted() {
                     @Override
                     public void onTaskCompleted(List s) {
@@ -56,6 +77,7 @@ public class SmscMainFragment extends Fragment {
                         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, s);
                         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                         smscList.setAdapter(dataAdapter);
+                        showContentOrLoadingIndicator(true);
                     }
                 }).execute(smscList);
             }
@@ -68,6 +90,7 @@ public class SmscMainFragment extends Fragment {
         smscList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                showContentOrLoadingIndicator(false);
                 if (smscList.getSelectedItem().equals("smsc1")) {
                     executeURL = smscDetails;
                 } else if (smscList.getSelectedItem().equals("smsc2")) {
@@ -81,6 +104,8 @@ public class SmscMainFragment extends Fragment {
                         smscStates.setText((s.get(0).toString()));
                         activeSessions.setText((s.get(1).toString()));
                         throughtPut.setText((s.get(2).toString()));
+
+                        showContentOrLoadingIndicator(true);
                     }
                 }).execute(executeURL);
             }
@@ -96,16 +121,52 @@ public class SmscMainFragment extends Fragment {
 
             @Override
             public void onClick(View v) {
+                showContentOrLoadingIndicator(false);
                 new AsyncTasksManager(new OnTaskCompleted() {
                     @Override
                     public void onTaskCompleted(List s) {
                         ListView smscSessionList = (ListView) rootview.findViewById(R.id.smscSessionlistView);
                         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, s);
                         smscSessionList.setAdapter(dataAdapter);
+                        showContentOrLoadingIndicator(true);
                     }
                 }).execute(smscSessionDetails);
             }
         });
+
         return rootview;
+    }
+
+    private void showContentOrLoadingIndicator(boolean contentLoaded) {
+        // Decide which view to hide and which to show.
+        final View showView = contentLoaded ? mContentView : mLoadingView;
+        final View hideView = contentLoaded ? mLoadingView : mContentView;
+
+        // Set the "show" view to 0% opacity but visible, so that it is visible
+        // (but fully transparent) during the animation.
+        showView.setAlpha(0f);
+        showView.setVisibility(View.VISIBLE);
+
+        // Animate the "show" view to 100% opacity, and clear any animation listener set on
+        // the view. Remember that listeners are not limited to the specific animation
+        // describes in the chained method calls. Listeners are set on the
+        // ViewPropertyAnimator object for the view, which persists across several
+        // animations.
+        showView.animate()
+                .alpha(1f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(null);
+
+        // Animate the "hide" view to 0% opacity. After the animation ends, set its visibility
+        // to GONE as an optimization step (it won't participate in layout passes, etc.)
+        hideView.animate()
+                .alpha(0f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        hideView.setVisibility(View.GONE);
+                    }
+                });
     }
 }
